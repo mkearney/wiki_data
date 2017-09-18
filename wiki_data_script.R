4547e6c98c462f54d8a32907cdf19d46031ef2bc
Michael W. Kearney <kearneymw@missouri.edu>
2017-09-18 15:47:18 -0500

    rproj and annotationed script with data source URL

--- wiki_data_script.R
+++ wiki_data_script.R
@@ -19,7 +19,11 @@ source("read_wiki_data.R")
 ##                      READ IN ENTIRE FILE OF WIKI DATA                      ##
 ##----------------------------------------------------------------------------##
 
-## name of saved data file
+
+## go here to download the file
+browseURL("https://dumps.wikimedia.org/other/pageviews/2017/2017-09/")
+
+## name/location of where you saved the data
 file_name <- "~/Downloads/pageviews-20170901-090000"
 
 ## read 1 million lines starting at 1700000
@@ -38,7 +42,7 @@ readr::write_csv(
 ##----------------------------------------------------------------------------##
 
 ## read csv data
-readr::read_csv(
+x <- readr::read_csv(
   "wiki-english-data.csv"
 )
 
@@ -48,13 +52,12 @@ search_keyword <- "interpersonal"
 ## see what topics include the key word
 grep(search_keyword, x$topic, ignore.case = TRUE, value = TRUE)
 
-## see rows for all matching keywords
+## see rows for all matching keywords (organized by number of pageviews)
 ip_topics <- x[grep(search_keyword, x$topic, ignore.case = TRUE), ]
-
-
-x[order(x$pageviews, decreasing = TRUE), ]
-
 ip_topics[order(ip_topics$pageviews, decreasing = TRUE), ]
+
+## organize all rows by pageviews
+x[order(x$pageviews, decreasing = TRUE), ]
 
 ## subset data for only one keyword
 keyword <- "Interpersonal_attraction"
